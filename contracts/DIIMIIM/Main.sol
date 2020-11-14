b9b9e3e843e4b07d94317c69fab261e9d28f4632
gheoneaemil5 <gheoneaemil@yahoo.com>
2020-11-14 19:08:02 +0200

    updates

--- contracts/DIIMIIM/Main.sol
+++ contracts/DIIMIIM/Main.sol
@@ -25,7 +25,6 @@ contract Lending {
     uint256 installmentFrequency; // how many weeks between each installment ( payment )
     uint256 loanEnd; // "the point when the loan is approved to the point when it must be paid back to the lender"
     uint256 numberOfInstallments; // the number of installments that the borrower must pay.
-    // reduced to 16 bits sized uint
     uint256 status; // the current status of the loan
     address[] nftAddressArray; // the adderess of the ERC721
     address payable borrower; // the address who receives the loan
@@ -40,29 +39,41 @@ contract Lending {
     _;
   }
 
+
+
   function getLoanStatus ( uint256 loanId ) external view onlyOwner returns(uint256) {
     return loans[loanId].status;
   }
+
+
 
   function setLtv(uint256 newLtv) external onlyOwner returns(uint256) {
     ltv = newLtv;
     return(ltv);
   }
 
+
+
   function setInterestRateToCompany(uint256 newInterestRateToCompany) external onlyOwner returns(uint256) {
     interestRateToCompany = newInterestRateToCompany;
     return(ltv);
   }
+
+
 
   function setInterestRateToLender(uint256 newInterestRateToLender) external onlyOwner returns(uint256) {
     interestRateToLender = newInterestRateToLender;
     return(ltv);
   }
 
+
+
   function _percent(uint numerator, uint denominator, uint precision) internal pure returns(uint256) {
     uint256 num = numerator * 10 ** (precision+1);
     return (( num / denominator) + 5) / 10;
   }
+
+
 
   function _transferLoanNftsToEscrow( address[] memory nftAddressArray , uint256[] memory nftTokenIdArray ) internal {
     uint256 length = nftAddressArray.length;
@@ -74,6 +85,8 @@ contract Lending {
         nftTokenIdArray[i]
       );
   }
+
+
 
   function createLoan(
     address[] calldata nftAddressArray,
@@ -107,8 +120,8 @@ contract Lending {
           numberOfInstallments,
           0,
           nftAddressArray,
-          address(0),
           msg.sender,
+          address(0),
           token
       )
     );
@@ -116,23 +129,24 @@ contract Lending {
     return id;
   }
 
+
+
   function chooseLoan( uint256 loanId ) external payable returns(uint256, uint256, uint256){
     require(loans[loanId].loanEnd >= now,"Loan validity expired");
-    require(loans[loanId].borrower == address(0),"Someone else payed for this loan before you");
+    require(loans[loanId].lender == address(0),"Someone else payed for this loan before you");
     require(loans[loanId].status == 0, "This loan is currently not ready for borrowers");
 
     // Check how much is payed
-    uint256 installmentAmount = loans[loanId].loanAmount / loans[loanId].numberOfInstallments;
-    require(msg.value == installmentAmount,"Not enough ether");
+    require(msg.value == loans[loanId].loanAmount,"Not enough ether");
 
-    // Send 99% to lender & 1% to company
+    // Send 99% to borrower & 1% to company
     // Floating point problem , impossible to send rational qty of ether ( debatable )
     // The rest of the wei is sent to company by default
-    loans[loanId].lender.transfer(installmentAmount / 100 * 99);
-    owner.transfer(installmentAmount - installmentAmount / 100 * 99);
+    loans[loanId].borrower.transfer(loans[loanId].loanAmount / 100 * 99);
+    owner.transfer(loans[loanId].loanAmount - loans[loanId].loanAmount / 100 * 99);
 
     // Borrower assigned , status is 1 ( approved ) + first installment ( payment ) completed
-    loans[loanId].borrower = msg.sender;
+    loans[loanId].lender = msg.sender;
     loans[loanId].status = 1;
     loans[loanId].loanEnd = now + loans[loanId].installmentFrequency * 1 weeks;
 
@@ -141,11 +155,58 @@ contract Lending {
   }
 
 
+
+  function cancelLoan( uint256 loanId ) external returns(uint256,uint256) {
+    require(loans[loanId].borrower == msg.sender,"You're not the borrower of this loan");
+    require(loans[loanId].lender == address(0),"Someone else payed for this loan before you");
+    require(loans[loanId].status == 0, "The loan has a lender , it cannot be cancelled");
+    
+    // We set its validity date as now
+    loans[loanId].loanEnd = now;
+
+    // We return the changed values
+    return (loans[loanId].status,loans[loanId].loanEnd);
+  }
+
+  function finishLoan( uint256 loanId ) external {
+    require(now >= loans[loanId].loanEnd, "The loan is not finished yet");
+    require(loans[loanId].borrower == msg.sender || loans[loanId].lender == msg.sender,"You're not part of this loan");
+    require(loans[loanId].lender != address(0),"Loan cancelled");
+    require(loans[loanId].status >= 1, "Loan cancelled");
+
+    // We check the loan items
+    uint256 length = loans[loanId].nftAddressArray.length;
+    require(length == loans[loanId].nftTokenIdArray.length, "Token infos provided are invalid");
+
+    // If all payments are done by the borrower
+    if ( loans[loanId].status == loans[loanId].numberOfInstallments ){
+
+      // We send the items back to him
+      for( uint256 i = 0; i < length; ++i ) 
+        IERC721(loans[loanId].nftAddressArray[i]).safeTransferFrom(
+          address(this),
+          loans[loanId].borrower,
+          loans[loanId].nftTokenIdArray[i]
+        );
+    }else{
+
+      // Otherwise the lender will receive the items
+      for( uint256 i = 0; i < length; ++i ) 
+        IERC721(loans[loanId].nftAddressArray[i]).safeTransferFrom(
+          address(this),
+          loans[loanId].lender,
+          loans[loanId].nftTokenIdArray[i]
+        );
+    }
+
+  }
+
   // The borrower can ask for a loan extension from the website , no blockchain operation required
   function extendLoan( uint256 loanId , uint256 nrOfWeeks ) external returns(uint256,uint256,uint256) {
-    require(loans[loanId].lender == msg.sender);
-    require(loans[loanId].status >= 1, "You can't extend a loan without an active borrower");
+    require(loans[loanId].lender == msg.sender,"You're not the lender of this loan");
+    require(loans[loanId].status >= 1, "You can't extend a loan without an active lender");
     require(loans[loanId].status < loans[loanId].numberOfInstallments, "All payments have been done for this loan");
+    require(loans[loanId].loanEnd >= now,"Loan validity expired");
     
     // Extend the loan finish date
     // The extension is automatically considered as paid installments ( complexity reasons ) ( debatable )
@@ -157,6 +218,8 @@ contract Lending {
     return (loans[loanId].loanEnd,loans[loanId].status,loans[loanId].numberOfInstallments);
   }
 
+
+
   function payLoan( uint256 loanId ) external payable returns(uint256, uint256, bool){
     require(loans[loanId].loanEnd >= now,"Loan validity expired");
     require(loans[loanId].borrower == msg.sender,"You're not the borrower of this loan");
@@ -164,23 +227,17 @@ contract Lending {
     require(loans[loanId].status < loans[loanId].numberOfInstallments, "All payments have been done for this loan");
     
     // Check how much is payed
-    uint256 installmentAmount = loans[loanId].loanAmount / loans[loanId].numberOfInstallments;
-    // Check if payment doesn't exceed
-    require(installmentAmount <= loans[loanId].numberOfInstallments - loans[loanId].status);
+    uint256 installmentAmount = ( loans[loanId].loanAmount + loans[loanId].interestRate ) / loans[loanId].numberOfInstallments;
     require(msg.value < installmentAmount,"Not enough ether");
-
-    // If interest rate can be greater than 1x installment amount >> BUG ( to discuss )
 
     // Check how many installments he's paying for
     uint256 totalPayments = msg.value / installmentAmount;
 
-    // If this is not the last installment
-    if ( loans[loanId].status < loans[loanId].numberOfInstallments - 1 )
-      // We check to have an exact qty of ether
-      require(totalPayments * installmentAmount == msg.value);
-    else
-      // Otherwise we check for both : last installment + interest rate 
-      require(totalPayments * installmentAmount + loans[loanId].interestRate == msg.value);
+    // Check if payment doesn't exceed
+    require(totalPayments <= loans[loanId].numberOfInstallments - loans[loanId].status,"You're trying to pay too much");
+
+    // We check to have an exact qty of ether
+    require(totalPayments * installmentAmount == msg.value);
 
     // Transfer the ether
     loans[loanId].lender.transfer( installmentAmount * totalPayments / 100 * 99);
@@ -198,5 +255,7 @@ contract Lending {
     // Return the date of payment , current status and the finished boolean status
     return( now , loans[loanId].status , isFinished );
   }
+
+
 
 }
