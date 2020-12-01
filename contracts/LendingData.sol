2271648afb0b6429a7b0f55337cc74d6974bcc55
raziehl <matei.petroschi@gmail.com>
2020-12-01 11:17:59 +0200

    Added experimental ABIEncoderV2 for debugging, not considered experimental as of 0.6.0

--- contracts/LendingData.sol
+++ contracts/LendingData.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
+pragma experimental ABIEncoderV2;
+
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
