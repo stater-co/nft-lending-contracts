92b6f0871328358f1904fd740eeba9a9c111efa1
gheoneaemil5 <gheoneaemil@yahoo.com>
2021-02-28 14:29:01 +0200

    some other fixes

--- contracts/LendingData.sol
+++ contracts/LendingData.sol
@@ -22,7 +22,7 @@ contract LendingData is ERC721Holder, ERC1155Holder, Ownable, ReentrancyGuard {
   uint256 public loanID;
   uint256 public ltv = 600; // 60%
   uint256 public installmentFrequency = 1;
-  TimeScale public installmentTimeScale = TimeScale.MINUTES;
+  TimeScale public installmentTimeScale = TimeScale.WEEKS;
   uint256 public interestRate = 20;
   uint256 public interestRateToStater = 40;
   event NewLoan(uint256 indexed loanId, address indexed owner, uint256 creationDate, address indexed currency, Status status, string creationId);
@@ -71,7 +71,7 @@ contract LendingData is ERC721Holder, ERC1155Holder, Ownable, ReentrancyGuard {
     string calldata creationId,
     TokenType[] memory nftTokenTypeArray
   ) external {
-    require(nrOfInstallments > 0, "Loan must include at least 1 installment");
+    require(nrOfInstallments > 1, "Loan must include at least 2 installment");
     require(loanAmount > 0, "Loan amount must be higher than 0");
 
     // Compute loan to value ratio for current loan application
