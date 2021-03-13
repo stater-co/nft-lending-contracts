0f30f6d8955550284e1d2456332db649632abfa4
gheoneaemil5 <gheoneaemil@yahoo.com>
2021-03-13 15:54:13 +0200

    promissory notes implementation

--- contracts/LendingData.sol
+++ contracts/LendingData.sol
@@ -52,7 +52,7 @@ contract LendingData is ERC721Holder, ERC1155Holder, Ownable {
     TokenType[] nftTokenTypeArray; // the token types : ERC721 , ERC1155 , ...
   }
   mapping(uint256 => Loan) public loans;
-  mapping(address => mapping(uint256 => bool)) public promissoryPermissions;
+  mapping(uint256 => address) public promissoryPermissions;
 
   constructor(address _nftAddress, address _promissoryNoteContractAddress, address[] memory _geyserAddressArray, uint256[] memory _staterNftTokenIdArray) {
     nftAddress = _nftAddress;
@@ -266,26 +266,25 @@ contract LendingData is ERC721Holder, ERC1155Holder, Ownable {
   }
   
   function promissoryExchange(uint256[] calldata loanIds, address payable newOwner) external {
-      require(msg.sender == promissoryNoteContractAddress,"You're not whitelisted to access this method");
-      for ( uint256 i = 0 ; i < loanIds.length ; ++i ){
+      for (uint256 i = 0; i < loanIds.length; ++i){
         require(loans[loanIds[i]].lender != address(0),"One of the loans is not approved yet");
-        require(promissoryPermissions[promissoryNoteContractAddress][loanIds[i]],"You're not allowed to perform this operation on loan");
+        require(promissoryPermissions[loanIds[i]] == msg.sender,"You're not allowed to perform this operation on loan");
         loans[loanIds[i]].lender = newOwner;
       }
   }
   
   function setPromissoryPermissions(uint256[] calldata loanIds) external {
-      for ( uint256 i = 0 ; i < loanIds.length ; ++i ){
+      for (uint256 i = 0; i < loanIds.length; ++i){
           require(loans[loanIds[i]].lender == msg.sender,"One of the loans is not approved yet");
-          promissoryPermissions[promissoryNoteContractAddress][loanIds[i]] = true;
+          promissoryPermissions[loanIds[i]] = promissoryNoteContractAddress;
       }
   }
 
   function calculateDiscount(address requester) public view returns(uint256){
-    for ( uint i = 0 ; i < staterNftTokenIdArray.length ; ++i )
+    for (uint i = 0; i < staterNftTokenIdArray.length; ++i)
 	    if ( IERC1155(nftAddress).balanceOf(requester,staterNftTokenIdArray[i]) > 0 )
 		    return uint256(100).div(discountNft);
-	  for ( uint256 i = 0 ; i < geyserAddressArray.length ; ++i )
+	  for (uint256 i = 0; i < geyserAddressArray.length; ++i)
 	    if ( Geyser(geyserAddressArray[i]).totalStakedFor(requester) > 0 )
 		    return uint256(100).div(discountGeyser);
 	  return 1;
@@ -363,7 +362,7 @@ contract LendingData is ERC721Holder, ERC1155Holder, Ownable {
 
   // Calculates loan to value ratio
   function _percent(uint256 numerator, uint256 denominator) internal pure returns(uint256) {
-    return numerator.mul(10 ** 4).div(denominator).add(5).div(10);
+    return numerator.mul(10000).div(denominator).add(5).div(10);
   }
 
   // Transfer items fron an account to another
