caffc9ffd41b8b13ea3184171d86feeeb386dacf
gheoneaemil5 <gheoneaemil@yahoo.com>
2021-03-20 15:53:50 +0200

    createLoan using delegatecall

--- contracts/LendingCore.sol
+++ contracts/LendingCore.sol
@@ -28,7 +28,6 @@ contract LendingMethodsContract is ERC721Holder, ERC1155Holder, Ownable {
     enum TimeScale{ MINUTES, HOURS, DAYS, WEEKS }
     TimeScale public installmentTimeScale = TimeScale.WEEKS;
     enum Status{ UNINITIALIZED, LISTED, APPROVED, DEFAULTED, LIQUIDATED, CANCELLED, WITHDRAWN }
-    enum TokenType{ ERC721, ERC1155 }
     event NewLoan(uint256 indexed loanId, address indexed owner, uint256 creationDate, address indexed currency, Status status, string creationId);
     event LoanApproved(uint256 indexed loanId, address indexed lender, uint256 approvalDate, uint256 loanPaymentEnd, Status status);
     event LoanCancelled(uint256 indexed loanId, uint256 cancellationDate, Status status);
@@ -51,19 +50,34 @@ contract LendingMethodsContract is ERC721Holder, ERC1155Holder, Ownable {
         uint256 paidAmount; // the amount that has been paid back to the lender to date
         uint256 defaultingLimit; // the number of installments allowed to be missed without getting defaulted
         uint256 nrOfPayments; // the number of installments paid
-        TokenType[] nftTokenTypeArray; // the token types : ERC721 , ERC1155 , ...
+        uint32[] nftTokenTypeArray; // the token types : ERC721 , ERC1155 , ...
     }
     mapping(uint256 => Loan) public loans;
     mapping(uint256 => address) public promissoryPermissions;
   
-    function setDiscounts(uint32 _discountNft, uint32 _discountGeyser, address[] calldata _geyserAddressArray, uint256[] calldata _staterNftTokenIdArray, address _nftAddress) public payable {
+    function setDiscounts(
+        uint32 _discountNft, 
+        uint32 _discountGeyser, 
+        address[] calldata _geyserAddressArray, 
+        uint256[] calldata _staterNftTokenIdArray, 
+        address _nftAddress
+    ) public payable {
         discountNft = _discountNft;
         discountGeyser = _discountGeyser;
         geyserAddressArray = _geyserAddressArray;
         staterNftTokenIdArray = _staterNftTokenIdArray;
         nftAddress = _nftAddress;
     }
-    function setGlobalVariables(address _promissoryNoteContractAddress, uint256 _ltv, uint256 _installmentFrequency, uint8 _installmentTimeScale, uint256 _interestRate, uint256 _interestRateToStater, uint32 _lenderFee) public payable {
+    
+    function setGlobalVariables(
+        address _promissoryNoteContractAddress, 
+        uint256 _ltv, 
+        uint256 _installmentFrequency, 
+        uint8 _installmentTimeScale, 
+        uint256 _interestRate, 
+        uint256 _interestRateToStater, 
+        uint32 _lenderFee
+    ) public payable {
         ltv = _ltv;
         installmentFrequency = _installmentFrequency;
         installmentTimeScale = TimeScale(_installmentTimeScale);
@@ -72,6 +86,93 @@ contract LendingMethodsContract is ERC721Holder, ERC1155Holder, Ownable {
         lenderFee = _lenderFee;
         promissoryNoteContractAddress = _promissoryNoteContractAddress;
     }
+    
+    // Borrower creates a loan
+    function createLoan(
+        uint256 loanAmount,
+        uint256 nrOfInstallments,
+        address currency,
+        uint256 assetsValue, 
+        address[] calldata nftAddressArray, 
+        uint256[] calldata nftTokenIdArray,
+        string calldata creationId,
+        uint32[] calldata nftTokenTypeArray
+    ) public payable {
+        require(nrOfInstallments > 0, "Loan must have at least 1 installment");
+        require(loanAmount > 0, "Loan amount must be higher than 0");
+        require(nftAddressArray.length > 0, "Loan must have atleast 1 NFT");
+        require(nftAddressArray.length == nftTokenIdArray.length && nftTokenIdArray.length == nftTokenTypeArray.length, "NFT provided informations are missing or incomplete");
+        
+        // Compute loan to value ratio for current loan application
+        require(_percent(loanAmount, assetsValue) <= ltv, "LTV exceeds maximum limit allowed");
+        
+        // Computing the defaulting limit
+        if ( nrOfInstallments <= 3 )
+            loans[loanID].defaultingLimit = 1;
+        else if ( nrOfInstallments <= 5 )
+            loans[loanID].defaultingLimit = 2;
+        else if ( nrOfInstallments >= 6 )
+            loans[loanID].defaultingLimit = 3;
+        
+        // Set loan fields
+        loans[loanID].nftTokenIdArray = nftTokenIdArray;
+        loans[loanID].loanAmount = loanAmount;
+        loans[loanID].assetsValue = assetsValue;
+        loans[loanID].amountDue = loanAmount.mul(interestRate.add(100)).div(100); // interest rate >> 20%
+        loans[loanID].nrOfInstallments = nrOfInstallments;
+        loans[loanID].installmentAmount = loans[loanID].amountDue.mod(nrOfInstallments) > 0 ? loans[loanID].amountDue.div(nrOfInstallments).add(1) : loans[loanID].amountDue.div(nrOfInstallments);
+        loans[loanID].status = Status.LISTED;
+        loans[loanID].nftAddressArray = nftAddressArray;
+        loans[loanID].borrower = msg.sender;
+        loans[loanID].currency = currency;
+        loans[loanID].nftTokenTypeArray = nftTokenTypeArray;
+        
+        // Transfer the items from lender to stater contract
+        _transferItems(
+            msg.sender, 
+            address(this), 
+            nftAddressArray, 
+            nftTokenIdArray,
+            nftTokenTypeArray
+        );
+        
+        // Fire event
+        emit NewLoan(loanID, msg.sender, block.timestamp, currency, Status.LISTED, creationId);
+        ++loanID;
+    }
+    
+    // Transfer items fron an account to another
+    function _transferItems(
+        address from, 
+        address to, 
+        address[] memory nftAddressArray, 
+        uint256[] memory nftTokenIdArray,
+        uint32[] memory nftTokenTypeArray
+    ) internal {
+        uint256 length = nftAddressArray.length;
+        require(length == nftTokenIdArray.length && nftTokenTypeArray.length == length, "Token infos provided are invalid");
+        for(uint256 i = 0; i < length; ++i) 
+        if ( nftTokenTypeArray[i] == 0 )
+            IERC721(nftAddressArray[i]).safeTransferFrom(
+                from,
+                to,
+                nftTokenIdArray[i]
+            );
+        else
+            IERC1155(nftAddressArray[i]).safeTransferFrom(
+                from,
+                to,
+                nftTokenIdArray[i],
+                1,
+                '0x00'
+            );
+    }
+    
+    // Calculates loan to value ratio
+    function _percent(uint256 numerator, uint256 denominator) internal pure returns(uint256) {
+        return numerator.mul(10000).div(denominator).add(5).div(10);
+    }
+    
 }
 
 contract LendingCore is ERC721Holder, ERC1155Holder, Ownable {
@@ -93,7 +194,6 @@ contract LendingCore is ERC721Holder, ERC1155Holder, Ownable {
   enum TimeScale{ MINUTES, HOURS, DAYS, WEEKS }
   TimeScale public installmentTimeScale = TimeScale.WEEKS;
   enum Status{ UNINITIALIZED, LISTED, APPROVED, DEFAULTED, LIQUIDATED, CANCELLED, WITHDRAWN }
-  enum TokenType{ ERC721, ERC1155 }
   event NewLoan(uint256 indexed loanId, address indexed owner, uint256 creationDate, address indexed currency, Status status, string creationId);
   event LoanApproved(uint256 indexed loanId, address indexed lender, uint256 approvalDate, uint256 loanPaymentEnd, Status status);
   event LoanCancelled(uint256 indexed loanId, uint256 cancellationDate, Status status);
@@ -116,7 +216,7 @@ contract LendingCore is ERC721Holder, ERC1155Holder, Ownable {
     uint256 paidAmount; // the amount that has been paid back to the lender to date
     uint256 defaultingLimit; // the number of installments allowed to be missed without getting defaulted
     uint256 nrOfPayments; // the number of installments paid
-    TokenType[] nftTokenTypeArray; // the token types : ERC721 , ERC1155 , ...
+    uint32[] nftTokenTypeArray; // the token types : ERC721 , ERC1155 , ...
   }
   mapping(uint256 => Loan) public loans;
   mapping(uint256 => address) public promissoryPermissions;
@@ -139,14 +239,15 @@ contract LendingCore is ERC721Holder, ERC1155Holder, Ownable {
     address[] calldata nftAddressArray, 
     uint256[] calldata nftTokenIdArray,
     string calldata creationId,
-    TokenType[] memory nftTokenTypeArray
-  ) external {
-        lendingMethodsContract.delegatecall(
+    uint32[] calldata nftTokenTypeArray
+  ) public payable {
+        (bool success, ) = lendingMethodsContract.delegatecall(
             abi.encodeWithSignature(
-                "createLoan(uint256,uint256,address,uint256,address[] calldata,uint256[] calldata, string calldata,TokenType[] memory)",
+                "createLoan(uint256,uint256,address,uint256,address[],uint256[],string,uint32[])",
                 loanAmount,nrOfInstallments,currency,assetsValue,nftAddressArray,nftTokenIdArray,creationId,nftTokenTypeArray
             )
         );
+        require(success,"Failed to createLoan via delegatecall");
   }
 
   // Lender approves a loan
@@ -297,42 +398,6 @@ contract LendingCore is ERC721Holder, ERC1155Holder, Ownable {
   
   function addNftTokenId(uint256 nftId) external onlyOwner {
       staterNftTokenIdArray.push(nftId);
-  }
-
-  // Calculates loan to value ratio
-  function _percent(uint256 numerator, uint256 denominator) internal pure returns(uint256) {
-    return numerator.mul(10000).div(denominator).add(5).div(10);
-  }
-
-  // Transfer items fron an account to another
-  function _transferItems(
-    address from, 
-    address to, 
-    address[] memory nftAddressArray, 
-    uint256[] memory nftTokenIdArray,
-    TokenType[] memory nftTokenTypeArray
-  ) internal {
-        lendingMethodsContract.delegatecall(
-            abi.encodeWithSignature(
-                "transferItems(address,address,address[] memory,uint256[] memory,TokenType[] memory)",
-                from,to,nftAddressArray,nftTokenIdArray,nftTokenTypeArray
-            )
-        );
-  }
-
-  function _transferTokens(
-      address from,
-      address payable to,
-      address currency,
-      uint256 qty1,
-      uint256 qty2
-  ) internal {
-        lendingMethodsContract.delegatecall(
-            abi.encodeWithSignature(
-                "transferTokens(address,address,address,uint256,uint256)",
-                from,to,currency,qty1,qty2
-            )
-        );
   }
 
 }
