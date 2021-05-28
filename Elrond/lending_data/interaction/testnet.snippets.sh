063dd6a0faab23d6c700659509515d82d01bf5d8
Gheonea Emilian <gheoneaemilian@pop-os.localdomain>
2021-05-28 17:02:16 +0300

    successfully communicating with testnet

--- Elrond/lending_data/interaction/testnet.snippets.sh
+++ Elrond/lending_data/interaction/testnet.snippets.sh
@@ -50,31 +50,77 @@ interestRateToStater() {
     erdpy --verbose contract query ${ADDRESS} --function="interestRateToStater" --proxy=${PROXY}
 }
 
+numberToElrondHex() {
+    nr=$1;
+    x=$( printf "%x" $nr );
+    stringified="$(printf "%q " "${x}")";
+    thelength=${#stringified};
+    if [[ $((($thelength-1) % 2)) -eq 0 ]];
+    then 
+        formattedLength="$((($thelength-1)/2))";
+    else
+        formattedLength="$(((($thelength-1)/2)+1))";
+    fi
+    finalFormatedHex=$(printf "%.8d\n" $formattedLength);
+    LEN=$(echo ${#x});
+    if [[ $((($LEN) % 2)) -eq 0 ]];
+    then
+        echo "x length % 2 == 0 , "$LEN" , "$x;
+        finalFormatedHex+=$x;
+    else
+        echo "x length % 2 != 0 , "$LEN" , "$x;
+        finalFormatedHex+="0"$x;
+    fi
+}
+
 createTheLoan() {
-    : '
     # LOAN_ARGUMENTS : 6000000 5 erd1qqqqqqqqqqqqqpgqn7kmy58sfnx2x5h7gxvlc20jnskcmy62d8ss9vk98j 10000000 [] [] []
     read -p "Enter the loan amount: " LOAN_AMOUNT
     read -p "Enter the number of installments: " NR_OF_INSTALLMENTS
     read -p "Enter the currency: " CURRENCY
     read -p "Enter the assets value: " ASSETS_VALUE
-    read -a "Enter the NFT address array: " NFT_ADDRESS_ARRAY
-    read -a "Enter the NFT token ID array: " NFT_TOKEN_ID_ARRAY
-    read -a "Enter the NFT token type array: " NFT_TOKEN_TYPE_ARRAY
-    CURRENCY="0x$(erdpy wallet bech32 --decode ${CURRENCY})"
-    FORMATTED_NFT_ADDRESS_ARRAY = "0x"
-    FORMATTED_NFT_TOKEN_ID_ARRAY = "0x"
-    FORMATTED_NFT_TOKEN_TYPE_ARRAY = "0x"
-    '
-    : '
-    for i in $NFT_ADDRESS_ARRAY
-        FORMATTED_NFT_ADDRESS_ARRAY .= $(erdpy wallet bech32 --decode ${i})
-    for i in $NFT_TOKEN_ID_ARRAY
-        FORMATTED_NFT_TOKEN_ID_ARRAY .= $(erdpy wallet bech32 --encode ${i})
-    for i in $NFT_TOKEN_TYPE_ARRAY
-        FORMATTED_NFT_TOKEN_TYPE_ARRAY .= $(erdpy wallet bech32 --encode ${i})
-    '
+    
+    
+    # Example : erd1qqqqqqqqqqqqqpgqn7kmy58sfnx2x5h7gxvlc20jnskcmy62d8ss9vk98j erd1qqqqqqqqqqqqqpgqn7kmy58sfnx2x5h7gxvlc20jnskcmy62d8ss9vk98j
+    echo "Enter the NFT address array: "
+    read -a NFT_ADDRESS_ARRAY
+    
 
-    : '
+    # Example : 0 1    
+    echo "Enter the NFT token ID array: "
+    read -a NFT_TOKEN_ID_ARRAY
+    
+    
+    # Example : 1 1
+    echo "Enter the NFT token type array: "
+    read -a NFT_TOKEN_TYPE_ARRAY
+    
+    
+    CURRENCY="0x$(erdpy wallet bech32 --decode ${CURRENCY})"
+    FORMATTED_NFT_ADDRESS_ARRAY="0x"
+    FORMATTED_NFT_TOKEN_ID_ARRAY="0x"
+    FORMATTED_NFT_TOKEN_TYPE_ARRAY="0x"
+    
+    for i in ${NFT_ADDRESS_ARRAY[@]};
+    do
+        FORMATTED_NFT_ADDRESS_ARRAY+="$(erdpy wallet bech32 --decode $i)";
+    done
+
+    
+    for i in ${NFT_TOKEN_ID_ARRAY[@]};
+    do
+        numberToElrondHex $i;
+        FORMATTED_NFT_TOKEN_ID_ARRAY+=$finalFormatedHex;
+    done
+
+
+    for i in ${NFT_TOKEN_TYPE_ARRAY[@]};
+    do
+        numberToElrondHex $i;
+        FORMATTED_NFT_TOKEN_TYPE_ARRAY+=$finalFormatedHex;
+    done
+
+    echo ${LOAN_AMOUNT} ${NR_OF_INSTALLMENTS} ${CURRENCY} ${ASSETS_VALUE} ${FORMATTED_NFT_ADDRESS_ARRAY} ${FORMATTED_NFT_TOKEN_ID_ARRAY} ${FORMATTED_NFT_TOKEN_TYPE_ARRAY};
     erdpy \
     --verbose contract call ${ADDRESS} \
     --recall-nonce \
@@ -85,7 +131,6 @@ createTheLoan() {
     --send \
     --proxy=${PROXY} \
     --chain=T
-    '
 }
 
 loans() {
