d5b349a6faff7a4ad2969dab8a3de8c4f4202a17
DIIMIIM <emilian@stater.co>
2021-06-08 14:46:16 +0300

    deposit method

--- contracts/pooling/StaterPool.sol
+++ contracts/pooling/StaterPool.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.7.4;
+import "../libs/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../libs/openzeppelin-solidity/contracts/access/Ownable.sol";
 
 
-contract StaterPool {
+contract StaterPool is Ownable {
 
     uint256 public poolID;
     struct Pool {
@@ -16,14 +18,20 @@ contract StaterPool {
     mapping(uint256 => Pool) public pools;
 
     function createPool(address currency,uint256 quantity) external payable {
-        if ( currency == address(0) || msg.value > 0 ){
-            pools[poolID].currency = address(0);
+        require(currency == address(0) || msg.value > 0, "You have to either use ETH or a custom ERC20 token as pool currency");
+        pools[poolID].currency = currency;
+        if ( currency == address(0) && msg.value > 0 ){
             msg.value > 0 ? pools[poolID].funds[msg.sender] = msg.value : pools[poolID].funds[msg.sender] = quantity;
-        }else{
+        }else if( msg.value == 0 && currency != address(0) ){
             /*
              * @DIIMIIM : Transfer tokens from send to this here 
              */
             pools[poolID].currency = currency;
+            require(IERC20(currency).transferFrom(
+              msg.sender,
+              address(this), 
+              quantity
+          ), "Transfer of tokens to Stater Pool failed");
         }
             
         pools[poolID].participants = 1;
