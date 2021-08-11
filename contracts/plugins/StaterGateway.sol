f0ac90515bde675223ae86a2cef66b6eb9d19b78
DIIMIIM <emilian@stater.co>
2021-08-11 18:06:31 +0300

    Stater Protocol Gateway >> initial protocol structure

--- contracts/plugins/StaterGateway.sol
+++ contracts/plugins/StaterGateway.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.4;
-import "../libs/openzeppelin-solidity/contracts/access/Ownable.sol";
+pragma solidity 0.8.6;
 
-
-contract StaterGateway is Ownable {
+contract StaterGateway {
     
+    mapping(address => mapping(address => bytes4)) public boilerplate;
 
+    modifier guarded(address to) {
+        require(boilerplate[msg.sender][to].length > 0);
+    }
 
+    function vanillaCall(address to, bytes calldata input) external guarded(to) {
+
+    }
+
+    function delegateCall(address to, bytes calldata input) external guarded(to) {
+
+    }
+
+    function updateBoilerplate(address to, bytes4 method) external {
+        boilerplate[msg.sender][to] = method;
+    }
 
 }
