125600ecd8a16f76686c77436b937dd7b5398525
DIIMIIM <emilian@stater.co>
2021-08-11 17:39:37 +0300

    Stater Gateway Protocol skeleton

---
+++ contracts/plugins/StaterGateway.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.4;
+import "../libs/openzeppelin-solidity/contracts/access/Ownable.sol";
+
+
+contract StaterGateway is Ownable {
+    
+
+
+
+}
