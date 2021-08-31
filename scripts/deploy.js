a3b839c50015c1330052c3a7b317c076e7a336f2
DIIMIIM <emilian@stater.co>
2021-08-31 15:57:51 +0300

    nonce setter removed

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -23,32 +23,32 @@ async function main() {
   const Discounts = await hre.ethers.getContractFactory("StaterDiscounts");
   console.log('Discounts: Got Contract Factory');
   const sender = await hre.ethers.getSigner();
-  const initialNonce = await sender.getTransactionCount();
-  const discounts = await Discounts.deploy({ nonce: BigNumber.from(initialNonce).add(100) });
-  console.log('Discounts: Deployed Contract');
+  //const initialNonce = await sender.getTransactionCount();
+  const discounts = await Discounts.deploy();
+  console.log('Discounts: Deployed Contract ' + JSON.stringify(discounts));
 
   await discounts.deployed();
   console.log('Discounts: Got deployed contract >> ' + JSON.stringify(discounts));
 
-  await discounts.addDiscount(2,"0x1a08a4be4c59d808ee730d57a369b1c09ed62352",20,[], { nonce: BigNumber.from(initialNonce).add(200) });
+  await discounts.addDiscount(2,"0x1a08a4be4c59d808ee730d57a369b1c09ed62352",20,[]);
   console.log('Discounts: Add Geyser Discount >> ' + JSON.stringify(discounts));
-  await discounts.addDiscount(1,"0x4100670ee2f8aef6c47a4ed13c7f246e621228ec",2,[1,4,5,2], { nonce: BigNumber.from(initialNonce).add(300) });
+  await discounts.addDiscount(1,"0x4100670ee2f8aef6c47a4ed13c7f246e621228ec",2,[1,4,5,2]);
   console.log('Discounts: Add Stater Rarible tokens Discount >> ' + JSON.stringify(discounts));
 
   const PromissoryNote = await hre.ethers.getContractFactory("StaterPromissoryNote");
-  console.log('PromissoryNote: Got Contract Factory');
-  const promissoryNote = await PromissoryNote.deploy("Test Promissory Note","TPN", { nonce: BigNumber.from(initialNonce).add(400) });
-  console.log('PromissoryNote: Deployed Contract');
+  console.log('PromissoryNote: Got Contract Factory >> ' + JSON.stringify(PromissoryNote));
+  const promissoryNote = await PromissoryNote.deploy("Test Promissory Note","TPN");
+  console.log('PromissoryNote: Deployed Contract >> ' + JSON.stringify(promissoryNote));
 
   const LendingMethods = await hre.ethers.getContractFactory("LendingMethods");
-  console.log('LendingMethods: Got Contract Factory');
-  const lendingMethods = await LendingMethods.deploy({ nonce: BigNumber.from(initialNonce).add(500) });
-  console.log('LendingMethods: Deployed Contract');
+  console.log('LendingMethods: Got Contract Factory >> ' + JSON.stringify(LendingMethods));
+  const lendingMethods = await LendingMethods.deploy();
+  console.log('LendingMethods: Deployed Contract >> ' + JSON.stringify(lendingMethods));
 
   const LendingTemplate = await hre.ethers.getContractFactory("LendingTemplate");
-  console.log('LendingTemplate: Got Contract Factory');
-  const lendingTemplate = await LendingTemplate.deploy(promissoryNote.address,lendingMethods.address,discounts.address,{ nonce: BigNumber.from(initialNonce).add(600) });
-  console.log('LendingTemplate: Deployed Contract');
+  console.log('LendingTemplate: Got Contract Factory >> ' + JSON.stringify(LendingTemplate));
+  const lendingTemplate = await LendingTemplate.deploy(promissoryNote.address,lendingMethods.address,discounts.address);
+  console.log('LendingTemplate: Deployed Contract >> ' + JSON.stringify(lendingTemplate));
   await lendingTemplate.deployed();
   console.log('LendingTemplate: Got Deployed Contract >> ' + JSON.stringify(lendingTemplate));
 
@@ -56,7 +56,7 @@ async function main() {
   console.log('PromissoryNote: Got Deployed Contract >> ' + JSON.stringify(promissoryNote));
   await lendingMethods.deployed();
   console.log('LendingMethods: Got Deployed Contract >> ' + JSON.stringify(lendingMethods));
-  await deployedPromissoryNote.setLendingDataAddress(lendingTemplate.address,{ nonce: BigNumber.from(initialNonce).add(200) });
+  await deployedPromissoryNote.setLendingDataAddress(lendingTemplate.address);
   console.log('PromissoryNote: Set Lending Data Address >> ' + JSON.stringify(deployedPromissoryNote));
 
   console.log('\n=========================================================================\n')
