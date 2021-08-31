802f2d735c0f50a2186173fc3629cbdf01d5a7df
DIIMIIM <emilian@stater.co>
2021-08-31 14:40:13 +0300

    deployments script split

--- scripts/debug.js
+++ scripts/debug.js
@@ -21,28 +21,44 @@ async function main() {
 
   // We get the contract to deploy
   const Discounts = await hre.ethers.getContractFactory("StaterDiscounts");
-  const discounts = await Discounts.deploy();
-  await discounts.deployed();
-
-  await discounts.addDiscount(2,"0x1a08a4be4c59d808ee730d57a369b1c09ed62352",20,[]);
-  await discounts.addDiscount(1,"0x4100670ee2f8aef6c47a4ed13c7f246e621228ec",2,[1,4,5,2]);
-
   const PromissoryNote = await hre.ethers.getContractFactory("StaterPromissoryNote");
+  const NFT721 = await hre.ethers.getContractFactory("GameItems721");
+  const NFT1155 = await hre.ethers.getContractFactory("GameItems1155");
+  const StakingTokens = await hre.ethers.getContractFactory("StakingTokens");
+  const DistributionTokens = await hre.ethers.getContractFactory("DistributionTokens");
+  const discounts = await Discounts.deploy();
+  const nft721 = await NFT721.deploy();
+  const nft1155 = await NFT1155.deploy();
+  const stakingTokens = await StakingTokens.deploy(BigNumber.from('1000000000000000000'),"Test Staking Tokens", "TST");
+  const distributionTokens = await DistributionTokens.deploy(BigNumber.from('1000000000000000000'),"Test Distribution Tokens", "TDT");
   const promissoryNote = await PromissoryNote.deploy("Test Promissory Note","TPN");
-
   const LendingMethods = await hre.ethers.getContractFactory("LendingMethods");
   const lendingMethods = await LendingMethods.deploy();
+
+  await discounts.deployed();
+  await nft721.deployed();
+  await nft1155.deployed();
+  await stakingTokens.deployed();
+  await distributionTokens.deployed();
+  const deployedPromissoryNote = await promissoryNote.deployed();
+  await lendingMethods.deployed();
+  
+  const TokenGeyser = await hre.ethers.getContractFactory("TokenGeyser");
+  const tokenGeyser = await TokenGeyser.deploy(stakingTokens.address,distributionTokens.address,10000,100,1000,100);
+  await tokenGeyser.deployed();
 
   const LendingTemplate = await hre.ethers.getContractFactory("LendingTemplate");
   const lendingTemplate = await LendingTemplate.deploy(promissoryNote.address,lendingMethods.address,discounts.address);
   await lendingTemplate.deployed();
 
-  const deployedPromissoryNote = await promissoryNote.deployed();
-  await lendingMethods.deployed();
   deployedPromissoryNote.setLendingDataAddress(lendingTemplate.address);
 
-
   console.log("Discounts deployed to:", discounts.address);
+  console.log("NFT721 deployed to:", nft721.address);
+  console.log("NFT1155 deployed to:", nft1155.address);
+  console.log("Staking tokens deployed to:", stakingTokens.address);
+  console.log("Distribution tokens deployed to:", distributionTokens.address);
+  console.log("Token geyser deployed to:", distributionTokens.address);
   console.log("Promissory Note deployed to:" + promissoryNote.address);
   console.log("Lending methods deployed to:" + lendingMethods.address);
   console.log("Lending template deployed to:" + lendingTemplate.address);
