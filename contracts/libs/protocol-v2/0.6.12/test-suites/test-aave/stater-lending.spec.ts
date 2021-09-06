43eb824704e0fbfdb0f57e2ca6c7f56096a45e72
DIIMIIM <emilian@stater.co>
2021-09-06 22:20:37 +0300

    protocol-v2 scripts added

---
+++ contracts/libs/protocol-v2/0.6.12/test-suites/test-aave/stater-lending.spec.ts
@@ -0,0 +1,10 @@
+import { makeSuite, TestEnv } from './helpers/make-suite';
+const { expect } = require('chai');
+
+makeSuite('Stater Lending Routes', (testEnv: TestEnv) => {
+
+  it('Tries to deploy the health factor route', async () => {
+  
+  });
+
+});
