116cf5634be907fb54f7e3960fa141eae92a0859
DIIMIIM <emilian@stater.co>
2021-09-09 13:04:58 +0300

    adding NFT to protocol-v2

--- test-suites/test-aave/stater-lending.spec.ts
+++ test-suites/test-aave/stater-lending.spec.ts
@@ -3,8 +3,13 @@ const { expect } = require("chai");
 
 makeSuite("Stater Lending Routes", (testEnv: TestEnv) => {
   it("Tries to invoke mint not being the LendingPool", async () => {
-    const { staterHealthFactor } = testEnv;
-    //await pool.FLASHLOAN_PREMIUM_TOTAL();
-    await staterHealthFactor.MAX_SQRT_RATIO();
+    const { staterHealthFactor, usdc } = testEnv;
+    /*
+    let ok = await staterHealthFactor.id();
+    console.log("Lending pool is >> " + JSON.stringify(ok));
+    console.log("USDC address >> " + usdc.address);
+    let verify = await staterHealthFactor.whitelistedCurrencies("0xB29A60829E73C88f0A289c0000981245Bca2eAa4");
+    console.log("Verified >> " + JSON.stringify(verify));
+    */
   });
 });
